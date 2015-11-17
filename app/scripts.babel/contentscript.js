0451bf3b4d4adfc3fc4ac3e508ca72fba35ec65d
Alex Mishyn <mishyn@gmail.com>
2015-11-17 11:03:43 +0100

    add mute unmute

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,50 +2,10 @@
 
 class View {
 
-  isMuted() {
-    return $('.volumeControlFocus').hasClass('muted') || $('.selfView .micButton').hasClass('micButtonOff')
-
-  }
-
-  updateMuteIndicator() {
-    if (this.isMuted()) {
-      $('#muteIndicator').css({'background-color': 'red'})
-    } else {
-      $('#muteIndicator').css({'background-color': 'green'})
-    }
-  }
-
-  updateState() {
-    this.updateMuteIndicator()
-    this.showRabbitControl()
-  }
-
-  mute() {
-      $('.volumeControlFocus:not(.muted)').click()
-      $('.selfView .micButton.micButtonOn').click()
-
-    this.updateState()
-  }
-
-  unmute() {
-    $('.volumeControlFocus.muted').click()
-    $('.selfView .micButton.micButtonOff').click()
-    this.updateState()
-  }
-
-  showRabbitControl() {
-    $('.controlFocus').removeClass('hideAddFriend hideAddFriendPending hideKickout hideMuteVideo hideVolume hideMessage')
-  }
-
   render() {
-    $('body').append('<div id="muteControlCenter"><div id="reactControl"></div><div id="muteIndicator">Muted</div> <button id="mute">Mute</button><button id="unmute">UnMute</button> </div>')
+    $('body').append('<div id="muteControlCenter"><div id="reactControl"></div></div>')
     $('#muteControlCenter').css({'position': 'absolute', top: 0, right: 0, 'z-index': 1000, margin: 20})
-    $('#mute').click(this.mute.bind(this))
-    $('#unmute').click(this.unmute.bind(this))
-
-    this.updateState()
 
     ReactDOM.render(React.createElement(HelloMessage, { name: "John React" }), document.getElementById('reactControl'));
-
   }
 }
--- app/scripts.babel/hello_message.jsx
+++ app/scripts.babel/hello_message.jsx
@@ -1,21 +1,44 @@
-import { createStore } from 'redux'
-
-function controller(state = {}, action) {
-  switch (action.type) {
-    case 'MUTE':
-      return state.isMuted = true
-    default:
-      return state
-  }
-}
-
-let store = createStore(controller, {})
 
 // actions
+
+// function mutePinned() {
+//   $('.volumeControlFocus:not(.muted)').click()
+// }
+//
+// function unmutePinned() {
+//   $('.volumeControlFocus.muted').click()
+// }
+
 function mute() {
-  $('.volumeControlFocus:not(.muted)').click()
-  $('.selfView .micButton.micButtonOn').click()
-  return { type: 'ADD_TODO' }
+  $('.selfView .selfSessionMenuView .micButton.micButtonOn').click()
+}
+
+function unmute() {
+  $('.selfView .selfSessionMenuView .micButton.micButtonOff').click()
+}
+
+function showRabbitControls() {
+  $('.controlFocus').removeClass('hideAddFriend hideAddFriendPending hideKickout hideMuteVideo hideVolume hideMessage')
+  $('.userSessionBubble').addClass('showMenu').addClass('showUserName')
+}
+
+function isMuted() {
+  return $('.volumeControlFocus').hasClass('muted') || $('.selfView .micButton').hasClass('micButtonOff')
+}
+
+function isAllMuted() {
+  return $('.sessionsRight .menuItem.volume:not(.muted)').length == 0
+}
+
+function muteAllSessinons() {
+  console.log('mute them')
+
+  $('.sessionsRight .menuItem.volume .volumeIcon').click()
+}
+
+function unMuteAllSessinons() {
+  console.log('unmute them')
+  $('.sessionsRight .menuItem.volume.muted .volumeIcon').click()
 }
 
 
@@ -23,26 +46,49 @@ class HelloMessage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isMuted: false
+      isMuted:    isMuted(),
+      isAllMuted: isAllMuted()
     }
   }
 
   toggleMute() {
-    store.dispatch(mute)
-    this.setState({isMuted: !this.state.isMuted})
-  }
-
-  muteStyle() {
-    return {
-      backgroundColor: this.state.isMuted ? 'green' : 'red'
+    if (this.state.isMuted) {
+      unmute()
+      this.setState({isMuted: false})
+    } else {
+      mute()
+      this.setState({isMuted: true})
     }
   }
 
+  toggleMuteAll() {
+    if (this.state.isAllMuted) {
+      unMuteAllSessinons()
+      this.setState({isAllMuted: false})
+    } else {
+      muteAllSessinons()
+      this.setState({isAllMuted: true})
+    }
+  }
+
+  muteStyle(isMuted) {
+    return {
+      backgroundColor: isMuted ? 'red' : 'green'
+    }
+  }
+
+  showControls() {
+    showRabbitControls()
+  }
+
   render() {
-    let {isMuted} = this.state
+    showRabbitControls()
+    let {isMuted,isAllMuted} = this.state
     return (
       <div>
-        <button style={this.muteStyle()} onClick={this.toggleMute.bind(this)}>{ isMuted ? 'Unmute' : 'Mute' }</button>
+        <button style={this.muteStyle(this.state.isMuted)} onClick={this.toggleMute.bind(this)}>{ isMuted ? 'Unmute Me' : 'Mute Me' }</button>
+        <button style={this.muteStyle(this.state.isAllMuted)} onClick={this.toggleMuteAll.bind(this)}>{ isAllMuted ? 'Unmute Them' : 'Mute Them' }</button>
+          <button onClick={this.showControls.bind(this)}>Show controls</button>
       </div>
     )
   }
